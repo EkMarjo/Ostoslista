66661afe570665ba92f8a1843b6d9ee5adf546ac
Marjo <93marjo.ek@gmail.com>
2024-02-01 12:57:21 +0200

    Ostoslista tehtävä

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [message, setMessage] = useState('');
+const [messages, setMessages] = useState ([]);
+
+  const handleAdd = () => {
+    setMessages([...messages,message]);
+    setMessage('');
+  }
+  const handleClear = () => {
+    setMessages([]);
+  }
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+<View style={styles.container}>
+      <View style={{felx: 1}}>
+      <TextInput 
+      placeholder='Syötä teksti'
+      onChangeText={text => setMessage(text)}
+      value={message}
+      />
+     </View>
+     <View style= {styles.fixToText}>
+      <Button title ="Add" onPress= {handleAdd}/>
+      <Button title ="Clear" onPress= {handleClear}/>
+      </View>
+      <View style = {{ flex: 8}}>
+      <Text style={styles.headerText}>Shopping List</Text>
+      <FlatList
+      data={messages}
+      renderItem={({item})=> 
+      <View style={styles.listItem}>
+        
+      <Text>{item}</Text>
+      </View>
+    }
+      />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  }, 
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  }
+
 });
